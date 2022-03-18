1fb7727959db5b36dc58998a820b5e2214bdbb72
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-03-18 12:50:35 -0400

    commit tree fc80507247c64d266d867a2d91419fadc63e8317

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,32 @@
 // create your App component here
+import React, { useEffect, useState } from 'react'
+
+const App = () => {
+    const [dogPic, setDogPic] = useState(''); //set to "null" will also work
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(data => {
+            //console.log(data.message)
+            setDogPic(data.message)
+        });
+    }, []); //empty dependencies arr
+    
+    //Display a <p> tag with the text of "Loading..." when the component is first rendered   
+    if(!dogPic) return <p>Loading...</p>
+
+    return <img src={dogPic} alt="A Random Dog" />
+
+    // Refactored from:
+    // if(!dogPic === true) {
+    //     return <p>Loading...</p>;
+    // } else {
+    //     return <img src={dogPic} alt="A Random Dog" />
+    // }
+
+}
+
+
+
+export default App
